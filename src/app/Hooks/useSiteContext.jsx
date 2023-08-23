0c40c8c015df4d0fa076f459ed2d930b513692e3
frankperez-github <fp848584@gmail.com>
2023-08-23 10:51:12 -0400

    setting firebase configuration

--- src/app/Hooks/useSiteContext.jsx
+++ src/app/Hooks/useSiteContext.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react"
 import clients from "../../../public/clients.json"
-import products from "../../../public/products.json"
+import {db} from "@/app/firebase.config"
+import {getDocs, collection, snapshotEqual, } from "firebase/firestore"
 
 export default function useSiteContext()
 {
+    const [products, setProducts]=useState([])
+
+    const productsRef = collection(db, 'products')
+    getDocs(productsRef)
+        .then((snapshot)=>
+        {
+            setProducts(snapshot.docs)
+        })
     return {clients, products}
 }
